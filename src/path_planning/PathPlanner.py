85f4a0dd99ec5d980b740faf404bf0926ac76ee6
jigglestrudel <tomaszkrepa@outlook.com>
2025-11-25 01:04:42 +0100

    merged path planning

--- src/path_planning/PathPlanner.py
+++ src/path_planning/PathPlanner.py
@@ -17,6 +17,7 @@ class PathPlanner:
         self.img_path: str = None
         self.starting_point: np.array = None
         self.starting_direction: float = None
+        self.priority_field: np.array = None
         os.makedirs('src/path_planning/results', exist_ok=True)
 
     def run_path_finding(self):
@@ -24,7 +25,9 @@ class PathPlanner:
             raise RuntimeError("PathPlanner No algorithm")
         if self.img_path is None or self.starting_direction is None or self.starting_point is None:
             raise RuntimeError("PathPlanner No algorithm params")
-        self._path, self._directions, self._turns = self.algorithm.calculate_path(self.img_path, self.starting_point, self.starting_direction)
+        self._path, self._directions, self._turns = self.algorithm.calculate_path(self.img_path, self.starting_point,
+                                                                                  self.starting_direction,
+                                                                                  self.priority_field)
 
     def draw_path(self, merged_area: np.array):
         if self.img_path is not None:
@@ -55,3 +58,21 @@ class PathPlanner:
         self.starting_point = [centroid_x, centroid_y]
         self.starting_direction = starting_direction
         self._path, self._directions, self._turns = self.algorithm.calculate_path_detected_area(contours, hierarchy, self.starting_point, self.starting_direction)
+
+    def smoothen_path(self):
+        q_points = self._path * 0.75 + np.r_[[self.starting_point], self._path[:-1]] * 0.25
+        r_points = self._path * 0.25 + np.r_[[self.starting_point], self._path[:-1]] * 0.75
+        self._path = np.empty((2 * len(self._path), 2))
+        self._path[1::2] = q_points
+        self._path[0::2] = r_points
+
+    def calculate_path_score(self) -> (float, float):
+        lengths = np.sqrt(np.sum(np.diff(self._path, axis=0)**2, axis=1))
+        total_length = np.sum(lengths)
+
+        number_of_no_turns = np.count_nonzero((self._turns > -0.1) & (self._turns < 0.1))
+        number_of_small_turns = np.count_nonzero((self._turns > -np.pi/3) & (self._turns < np.pi/3)) - number_of_no_turns
+        number_of_big_turns = np.count_nonzero(self._turns) - number_of_small_turns - number_of_small_turns
+
+        turn_score = number_of_small_turns * 0.5 + number_of_big_turns
+        return total_length, turn_score
